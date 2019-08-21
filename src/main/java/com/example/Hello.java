0ddd03b17cb6bfcf3376525fc9a45de81a866c29
hudingtao <hu_dingtao@163.com>
2019-08-21 19:29:10 +0800

    初始化项目

---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author xiaotao
+ * @version 1.0
+ * @date: Created in 2019/8/21 19:23
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
